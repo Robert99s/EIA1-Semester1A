65e322614902dbf6977f60e036cdfc8f8d5b8389
Robert99s <102170260+Robert99s@users.noreply.github.com>
2022-05-22 18:00:00 +0200

    Aufgabe8final

---
+++ Aufgabe8/script.ts
@@ -0,0 +1,40 @@
+
+const sounds: HTMLAudioElement[] = 
+[
+    new Audio("assets/hihat.mp3"),
+    new Audio("assets/kick.mp3"),
+    new Audio("assets/snare.mp3"),
+    new Audio("assets/A.mp3"), 
+    new Audio("assets/C.mp3"), 
+    new Audio("assets/F.mp3"),
+    new Audio("assets/G.mp3"),
+    new Audio("assets/laugh-1.mp3"),
+    new Audio("assets/laugh-2.mp3")
+];
+
+// 
+function playSample(sound) {
+    sound.play();
+}
+
+// Playbutton-Event
+function playBeat() {
+    setInterval(
+        function(){
+        for (let counter = 0; counter < sounds.length; counter++)
+        {sounds[counter].play();}
+    }, 500);}
+
+    // Klick-Event
+    document.querySelector("#hihat").addEventListener("click", function() {playSample(sounds[0])});
+    document.querySelector("#kick").addEventListener("click", function() {playSample(sounds[1])});
+    document.querySelector("#snare").addEventListener("click", function() {playSample(sounds[2])});
+    document.querySelector("#noteA").addEventListener("click", function() {playSample(sounds[3])});
+    document.querySelector("#noteB").addEventListener("click", function() {playSample(sounds[4])});
+    document.querySelector("#noteC").addEventListener("click", function() {playSample(sounds[5])});
+    document.querySelector("#noteD").addEventListener("click", function() {playSample(sounds[6])});
+    document.querySelector("#laughter1").addEventListener("click", function() {playSample(sounds[7])});
+    document.querySelector("#laughter2").addEventListener("click", function() {playSample(sounds[8])});
+
+    // Klick-Event Playbutton
+    document.querySelector("#playbutton").addEventListener("click", function() {playBeat()});
