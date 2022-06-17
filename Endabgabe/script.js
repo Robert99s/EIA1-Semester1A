a863c4d1d492415dbefeda1b5791c81c2a05d50f
Robert99s <102170260+Robert99s@users.noreply.github.com>
2022-06-17 15:37:59 +0200

    EndabgabeV1

---
+++ Endabgabe/script.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function () {
+    let score = 0;
+    document.querySelector("#leicht").addEventListener("click", easy);
+    function easy() {
+    }
+});
+//# sourceMappingURL=script.js.map
---
+++ Endabgabe/script.ts
@@ -0,0 +1,9 @@
+window.addEventListener("load", function(): void {
+    let score: number = 0;
+
+    document.querySelector("#leicht").addEventListener("click", easy);
+
+    function easy (): void {
+
+    }
+});
